e75345dffe8b03f102a3f970ae352108a6916e94
klausmenendez <menendek@oregonstate.edu>
2022-10-05 10:51:57 -0700

    myproject

---
+++ average.py
@@ -0,0 +1,8 @@
+print ("Please Enter 5 numbers ")
+num1=float(input())
+num2=float(input())
+num3=float(input())
+num4=float(input())
+num5=float(input())
+sum=num1+num2+num3+num4+num5
+print("Their average is", sum/5)
