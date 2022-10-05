6964269bab907ed669aa7e4c4753f80260149746
klausmenendez <menendek@oregonstate.edu>
2022-10-05 10:26:56 -0700

    myproject

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+print ("Please Enter 5 numbers ")
+num1=float(input())
+num2=float(input())
+num3=float(input())
+num4=float(input())
+num5=float(input())
+sum=num1+num2+num3+num4+num5
+print("Their average is", sum/5)
+
+
+
